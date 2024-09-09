506832916712dce1d23af21ad2dfffb38e3348a0
Loogeek <xiaolong3956@gmail.com>
2024-09-09 18:30:35 +0800

    feat: add workspace

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
 "use client";
 
+import { useEffect, useMemo } from "react";
+
 import { UserButton } from "@/features/auth/components/user-button";
+import { useGetWorkSpaces } from "@/features/workspaces/api/use-get-workspaces";
+import { useCreateWorkSpaceModal } from "@/features/workspaces/store/use-create-workspace-modal";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useGetWorkSpaces();
+  const [open, setOpen] = useCreateWorkSpaceModal();
+
+  const workspaceId = useMemo(() => data?.[0]?._id, [data]);
+
+  useEffect(() => {
+    if (isLoading) return;
+
+    if (workspaceId) {
+      router.replace(`/workspace/${workspaceId}`);
+    } else if (!open) {
+      setOpen(true);
+    }
+  }, [workspaceId, isLoading, open, setOpen, router]);
+
   return (
     <div>
       <UserButton />
